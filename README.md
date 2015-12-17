06c1ca48a7c14129900908228b65125c2204e66b
Mike Kovacevich <mjkovacevich@2148310.no-reply.drupal.org>
2015-12-17 00:10:50 +0000

    Update changes made directly to website.

---
+++ README.md
@@ -0,0 +1,4 @@
+canalside
+=========
+
+A Symfony project created on September 9, 2015, 12:14 am.
---
+++ src/Canalside/Bundle/ServicesBundle/CanalsideServicesBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Canalside\Bundle\ServicesBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CanalsideServicesBundle extends Bundle
+{
+}
